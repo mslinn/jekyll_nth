69db272ce809778c5c9e314cb13415e07bb580a4
Mike Slinn <mslinn@mslinn.com>
2025-09-29 14:27:35 -0400

    -

--- spec/array_manipulation_spec.rb
+++ spec/array_manipulation_spec.rb
@@ -1,28 +1,36 @@
 require 'jekyll'
-require_relative '../lib/jekyll_nth'
+require_relative 'spec_helper'
 
 RSpec.describe(ArrayManipulation) do
   include described_class
-
-  let(:config) { instance_double(Configuration) }
-  let(:context) do
-    context_ = instance_double(Liquid::Context)
-    context_.config = config
-    context_
-  end
 
   it 'returns the usual Ruby value' do
     array = [1, 2, 3, 4, 5]
     expect(nth(array, -1)).to eq(5)
     expect(nth(array, 0)).to eq(1)
     expect(nth(array, 2)).to eq(3)
-    expect(nth(array, 99)).to raise_exception
-    expect(nth([], 1)).to raise_exception
+    expect { nth(array, 99) }
+      .to output(/.*/).to_stderr
+      .and raise_error(SystemExit) do |error|
+        expect(error.status).to eq(1)
+        expect(error.message).to match(/only 5 items, but item 99 was requested/)
+      end
+    expect { nth([], 1) }
+      .to output(/.*/).to_stderr
+      .and raise_error(SystemExit) do |error|
+        expect(error.status).to eq(1)
+        expect(error.message).to match(/array passed to nth was empty/)
+      end
   end
 
   it 'returns normal tail' do
     expect(tail([1, 2, 3, 4, 5])).to eq([2, 3, 4, 5])
     expect(tail([1])).to eq([])
-    expect(tail([])).to raise_exception
+    expect { tail([]) }
+      .to output(/.*/).to_stderr
+      .and raise_error(SystemExit) do |error|
+        expect(error.status).to eq(1)
+        expect(error.message).to match(/array passed to tail was empty/)
+      end
   end
 end
